4e0e00dac0cb864f733adab2a37a3e4cccaa9f78
amz56 <rijinamrutha@gmail.com>
2022-02-01 16:11:06 +0530

    new commit

---
+++ ComCast/src/main/java/createneworgTest/createorg.java
@@ -0,0 +1,16 @@
+package createneworgTest;
+
+import org.testng.annotations.Test;
+
+public class createorg {
+	@Test
+	
+	public void createOrgTest()
+	{
+		System.out.println("organization created");
+	}
+
+}
+
+
+
